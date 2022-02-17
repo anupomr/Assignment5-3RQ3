33275049d66ac318752273f1e54f0cf00b54ba4f
anupomr <anupomr@yahoo.com>
2022-02-16 23:15:19 -0500

    LabD: Login auto fill  confirmation created

---
+++ LabD.py
@@ -0,0 +1,12 @@
+# Name : Anupom Roy
+# Student No: 400350605
+
+class LabD:
+    def autofill_confrem_dilogbox(self):
+        return None
+    def save_user_confermation(self):
+        return None
+    def fill_login_info(self):
+        return None
+
+
